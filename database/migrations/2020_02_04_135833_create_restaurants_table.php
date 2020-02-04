5e70330b27d2d7ded5af6d6bca2c435847caba55
Jst <jsthtet96@gmail.com>
2020-02-05 06:06:55 +0630

    upload project

---
+++ database/migrations/2020_02_04_135833_create_restaurants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name', 100);
+            $table->string('location', 200);
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
